51e6118199bb53b9ed63718fe690ac76e95a0bff
Brecht <brechtnulens@gmail.com>
2020-10-18 18:05:15 +0200

    Starter file for main content

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <NewsArticleList/>
+  </main>
+</template>
+
+<script>
+import NewsArticleList from './NewsArticles/NewsArticlesList'
+
+export default {
+  components: {
+    NewsArticleList
+  }
+}
+</script>
+
+<style>
+</style>
