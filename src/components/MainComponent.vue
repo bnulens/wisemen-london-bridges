b2980a3917cea1b2e1ba9fc9a6fc8ecb8cc0f757
Brecht <brechtnulens@gmail.com>
2020-10-20 16:08:08 +0200

    Added MapBoxMap comp - logic for displaying NewsArticleList on click

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,18 +1,43 @@
 <template>
   <main>
-    <NewsArticleList/>
+    <NewsArticleList :class="isBridgeView ? 'show-news' : 'hide-news'"/>
+    <MapBoxMap/>
+    <!-- <Map/> -->
   </main>
 </template>
 
 <script>
 import NewsArticleList from './NewsArticles/NewsArticlesList'
+import MapBoxMap from './BridgeMap/MapBoxMap'
+// import Map from './BridgeMap/Map'
 
 export default {
   components: {
-    NewsArticleList
+    NewsArticleList,
+    MapBoxMap,
+    // Map
+  }, 
+  props: {
+    view: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    isBridgeView() {
+      if (this.view === 'bridges') {
+        return true
+      }
+      return false
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .show-news {
+    @media screen and (max-width: 768px) {
+      display: none;
+    }
+  }
 </style>
