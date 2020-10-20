53d9e88734ad4611d32812093ea1fc5bf8499be8
Brecht <brechtnulens@gmail.com>
2020-10-20 22:32:17 +0200

    Added alt attr to images - added separate CardInfo comps - check if accessibility = possible to render relevant icon

--- src/components/LocationCard/LocationCard.vue
+++ src/components/LocationCard/LocationCard.vue
@@ -5,24 +5,25 @@
                <div class="location-card-content-head">
                     <h2>{{ connectionToShow.name }}</h2>
                     <div class="location-card-content-head-info">
-                            <img src="@/assets/icons/warning.svg"/>
-                            <img src="@/assets/icons/info.svg"/>
-                            <button class="head-info-toggle-size">{{ this.view === 'collapsed' ? '&wedge;' : '&vee;'  }}</button>
+                        <img src="@/assets/icons/info.svg" alt="info-icon"/>
+                        <img src="@/assets/icons/warning.svg" alt="warning-icon"/>
+                        <button class="head-info-toggle-size">{{ this.view === 'collapsed' ? '&wedge;' : '&vee;' }}</button>
                     </div>
                </div>
                <div class="location-card-content-body">
-                    <LiftIconInfo/>
-                    <EscalatorIconInfo/>
+                    <LiftIconInfo :leftBankInfo="leftBankInfo" :rightBankInfo="rightBankInfo"/>
+                    <EscalatorIconInfo :leftBankInfo="leftBankInfo" :rightBankInfo="rightBankInfo"/>
+                    <CardInfo  icon="info" title="Liften" content="Wegens voorbereidende onderhoudswerken is de LO buiten dienst. Onze excuses voor het ongemak. fietsers wordt aangeraden gebruik te maken van de Millenium Bridge."/>
+                    <CardInfo  icon="warning" title="Algemeen" content="In het kader van de maatregelen tegen het CORONAvirus: HOU AUB VOLDOENDE AFSTAND"/>
+                    <div v-if="view !== 'collapsed'" class="accessibility-icons">
+                        <h3 class="location-card-content-subtitle">Toegankelijkheid</h3>
+                        <div class="accessibility-icons-wrapper">
+                            <span v-for="item in connectionToShow.accessibility" :key="item.id">
+                                <img v-if="item.possible" :src="getIcon(item)" alt="accessibility-icon"/>
+                            </span>
+                        </div>
+                    </div>
                </div>
-               <div class="accessibility-icons">
-                   <h3 class="location-card-content-subtitle">Toegankelijkheid</h3>
-                   <div class="accessibility-icons-wrapper">
-                       <span v-for="item in connectionToShow.accessibility" :key="item.id">
-                           <img :src="getIcon(item)" alt="accessibility-icon"/>
-                        </span>
-                   </div>
-                </div>
-                <!-- <span>{{ connectionToShow.connection_ends[0]["services"][0]["state"].name }}</span> -->
             </div>
         </section>
     </div>
@@ -32,11 +33,13 @@
 import axios from 'axios'
 import LiftIconInfo from './LiftIconInfo'
 import EscalatorIconInfo from './EscalatorIconInfo'
+import CardInfo from './CardInfo'
 
 export default {
     components: {
         LiftIconInfo,
-        EscalatorIconInfo
+        EscalatorIconInfo,
+        CardInfo
     },
     props: {
         connection: {
@@ -71,7 +74,7 @@ export default {
         rightBankInfo() {
             const filteredBanks = this.connectionToShow.connection_ends.filter(c => c.shore.short_name === 'RO');
             return filteredBanks[0]
-        },  
+        }
     },
     methods: {
         toggleView() {
@@ -107,14 +110,12 @@ export default {
 
         .location-card-content {
             position: relative;
-            padding-top: 10px;
             width: 100%;
         
             .location-card-content-head {
                 display: flex;
                 justify-content: space-between;
                 align-items: center;
-                width: 100%;
 
                 h2 {
                     color: $sec-color;
@@ -122,6 +123,10 @@ export default {
                 .location-card-content-head-info {
                     display: flex;
                     align-items: center;
+
+                    img {
+                        margin-left: 10px;
+                    }
                 }
                 .head-info-toggle-size {
                     display: block;
@@ -130,57 +135,82 @@ export default {
                     color: rgb(68, 68, 68);
                     background-color: transparent;
                 }
-                @media screen and (min-width: 768px) {
-                    height: 20vh;
-                }
             }
 
             .location-card-content-body {
                 margin: 12px 0px;
-            }
 
-            .accessibility-icons {
-                display: flex;
-                flex-direction: column;
-                h3 {
-                    font-size: 14px;
-                    font-weight: 400;
-                    display: block;
-                }
-                .accessibility-icons-wrapper {
-                    margin-top: 8px;
-                    span {
-                        display: inline-block;
-                        margin-right: 14px;
+                .accessibility-icons {
+                    display: flex;
+                    flex-direction: column;
+                    margin-top: 20px;
+
+                    h3 {
+                        font-size: 14px;
+                        font-weight: 400;
+                        display: block;
+                    }
+                    .accessibility-icons-wrapper {
+                        margin-top: 8px;
+                        span {
+                            display: inline-block;
+                            margin-right: 14px;
+                        }
                     }
                 }
             }
 
+
             @media screen and (min-width: 768px) {
-                position: absolute;
                 display: flex;
                 flex-direction: column;
                 padding-top: 0;
-                overflow: hidden;
             }
         }
 
         @media screen and (min-width: 768px) {
             position: absolute;
-            align-items: center;
             top: 5%;
             left: 65%;
-            height: 10%;
-            width: auto;
+            display: flex;
+            align-items: center;
+            height: 64px;
             border-radius: 20px;
         }
     }
 
     .location-card.expanded {
         bottom: 0;
+        padding-top: 13px;
+        transition: all ease 0.4s;
+
+        @media screen and (min-width: 768px) {
+            align-items: flex-start;
+            height: 480px;
+            padding-top: 13px;
+            transition: all ease 0.4s;
+        }
     }
     .location-card.collapsed {
         bottom: -320px;
+        padding-top: 13px;
+        transition: all ease 0.4s;
+
+        .location-card-content-body {
+            display: block;
+        }
+
+        @media screen and (min-width: 768px) {
+            align-items: flex-start;
+            height: 64px;
+            padding-top: 13px;
+            transition: all ease 0.4s;
+
+            .location-card-content-body {
+                display: none;;
+            }
+
+        }
     }
 
   }
