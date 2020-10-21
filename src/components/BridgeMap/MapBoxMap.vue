97d683d1eda81582352ee3771e581d26dc6a2417
Brecht <brechtnulens@gmail.com>
2020-10-21 03:37:17 +0200

    Set new center on map - added map bounds yielding excessive scroll - omitted styling from #map selector

--- src/components/BridgeMap/MapBoxMap.vue
+++ src/components/BridgeMap/MapBoxMap.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="container">
     <div class="map-container">
-      <!-- <LMap id="map" :center="center" :minZoom="minZoom" :maxZoom="maxZoom" :bounds="bounds" :max-bounds="maxBounds"> -->
-      <LMap id="map" :center="center" :minZoom="minZoom" :maxZoom="maxZoom">
+      <LMap id="map" :center="center" :minZoom="minZoom" :maxZoom="maxZoom" :bounds="bounds" :max-bounds="maxBounds">
         <LTileLayer :url="url"/>
       </LMap>
     </div>
@@ -13,7 +12,7 @@
 
 <script>
 import L from 'leaflet';
-// import { latLngBounds } from "leaflet";
+import { latLngBounds } from "leaflet";
 import { LMap, LTileLayer } from 'vue2-leaflet';
 import mapboxgl from 'mapbox-gl'
 
@@ -35,11 +34,11 @@ export default {
   },
   data() {
     return {
-      center: L.latLng(51.5101559, -0.0979333),
+      center: L.latLng(51.5123437, -0.0955912),
       minZoom: 14,
       maxZoom: 15,
-      // bounds: latLngBounds([[51.5029024,  -0.1146589],[51.5052242,  -0.1146131]]),
-      // maxBounds: latLngBounds([[51.5017936,  -0.1202588],[51.5052242,  -0.1146131]]),
+      bounds: latLngBounds([[51.5123000,  -0.0846589],[51.50000,  -0.1146131]]),
+      maxBounds: latLngBounds([[51.5123000,  -0.0846589],[51.50000,  -0.1146131]]),
       url:`http://api.mapbox.com/styles/v1/bnulens/ckggny7ao00u219o6zyntnmja/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYm51bGVucyIsImEiOiJja2dnbzRwcTcwMzc5MnFsajdqMGlwM2h3In0.tk7oltSZoRA3zTEcrAZmIA`,
     }
   }
@@ -51,16 +50,14 @@ export default {
 
   #container {
     .map-container {
-      display: block;
-      #map {
-        position: absolute;
-        top: 0;
-        height: 100%;
-        overflow: hidden;
-      }
-      @media screen and (max-width: 768px) {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+
+      @media screen and (min-width: 768px) {
         z-index: 0;
-        width: 100vw;
+        width: 70%;
+        height: 100vh;
       }
     }
   }
